dd3e9984a7f6ea4fc78e06d964cc59dee16a7b42
arinatechnologies <155801643+arinatechnologies@users.noreply.github.com>
2025-07-01 07:29:52 -0500

    Upload from Lambda

---
+++ js/about.js
@@ -0,0 +1,22 @@
+{
+  "hero_image": "modern office with technology equipment",
+  "team_photos": [
+    "professional cybersecurity expert",
+    "female technology executive",
+    "business technology leader",
+    "diverse technology team"
+  ],
+  "values_images": [
+    "cybersecurity shield icon",
+    "innovation lightbulb concept",
+    "team collaboration meeting",
+    "global network connection"
+  ],
+  "timeline_images": [
+    "startup garage workspace",
+    "business handshake meeting",
+    "venture capital funding",
+    "international office expansion",
+    "award ceremony business"
+  ]
+}
